723ba41bc1249e2cfdfc84beefaaca999555206a
labsesdt <labsesdt@gmail.com>
2020-03-16 16:47:46 +0800

    角色跳起來會回到地面

--- game4.10/Source/CHero.cpp
+++ game4.10/Source/CHero.cpp
@@ -43,6 +43,14 @@ namespace game_framework {
 		x = X_POS;
 		y = Y_POS;
 		isMovingLeft = isMovingRight = isMovingUp = isMovingDown = false;
+
+		const int INITIAL_VELOCITY = 20;	// 初始上升速度
+		const int FLOOR = 400;				// 地板座標
+		floor = FLOOR;
+		x = 95; y = FLOOR - 1;				// y座標比地板高1點(站在地板上)
+		rising = false;
+		initial_velocity = INITIAL_VELOCITY;
+		velocity = initial_velocity;
 	}
 
 	void CHero::LoadBitmap()
@@ -55,8 +63,9 @@ namespace game_framework {
 
 	void CHero::OnMove()
 	{
-		const int STEP_SIZE = 1;
-		animation.OnMove();
+		
+		const int STEP_SIZE = 5;
+		//animation.OnMove();
 		if (isMovingLeft)
 		{
 			x -= STEP_SIZE;
@@ -65,14 +74,38 @@ namespace game_framework {
 		{
 			x += STEP_SIZE;
 		}
-		if (isMovingUp)
+		if (isMovingUp && y == (floor-1))
 		{
-			y -= STEP_SIZE;
+			rising = true;
 		}
 		if (isMovingDown)
 		{
-			y += STEP_SIZE;
+			//y += STEP_SIZE;
 		}
+		/*
+		if (rising) {			// 上升狀態
+			if (velocity > 0) {
+				y -= velocity;	// 當速度 > 0時，y軸上升(移動velocity個點，velocity的單位為 點/次)
+				velocity--;		// 受重力影響，下次的上升速度降低
+			}
+			else {
+				rising = false; // 當速度 <= 0，上升終止，下次改為下降
+				velocity = 1;	// 下降的初速(velocity)為1
+			}
+		}
+		else {				// 下降狀態
+			if (y < floor - 1) {  // 當y座標還沒碰到地板
+				y += velocity;	// y軸下降(移動velocity個點，velocity的單位為 點/次)
+				velocity++;		// 受重力影響，下次的下降速度增加
+			}
+			else {
+				y = floor - 1;  // 當y座標低於地板，更正為地板上
+				//rising = true;	// 探底反彈，下次改為上升
+				velocity = initial_velocity; // 重設上升初始速度
+			}
+		}
+		*/
+		animation.OnMove();		// 執行一次animation.OnMove()，animation才會換圖
 	}
 
 	void CHero::SetMovingDown(bool flag)
