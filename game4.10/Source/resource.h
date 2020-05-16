e23af9186609a596d76021a7e83fe31635f45c62
shashishajin <t107590021@ntut.org.tw>
2020-05-16 18:04:47 +0800

    新增錢幣

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -316,6 +316,11 @@
 #define IDB_SWORDROLLLEFT_3             392
 #define IDB_BITMAP37                    393
 #define IDB_WHITE                       393
+#define IDB_BRONZECOIN_0                394
+#define IDB_BRONZECOIN_1                395
+#define IDB_BRONZECOIN_2                396
+#define IDB_BITMAP52                    397
+#define IDB_BRONZECOIN_3                397
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -328,7 +333,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        394
+#define _APS_NEXT_RESOURCE_VALUE        398
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
