6ff860bca6b803e8458f65e7f842c742b2d44617
shashishajin <t107590021@ntut.org.tw>
2020-06-18 20:54:02 +0800

    刪除不必要的滑鼠動作

--- game4.10/Source/mygame.h
+++ game4.10/Source/mygame.h
@@ -106,8 +106,6 @@ namespace game_framework {
 		void OnLButtonDown(UINT nFlags, CPoint point);  // 處理滑鼠的動作
 		void OnLButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
 		void OnMouseMove(UINT nFlags, CPoint point);	// 處理滑鼠的動作 
-		void OnRButtonDown(UINT nFlags, CPoint point);  // 處理滑鼠的動作
-		void OnRButtonUp(UINT nFlags, CPoint point);	// 處理滑鼠的動作
 	protected:
 		void OnMove();									// 移動遊戲元素
 		void OnShow();									// 顯示這個狀態的遊戲畫面
