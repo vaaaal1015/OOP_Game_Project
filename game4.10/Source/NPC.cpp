c0e0a80690547de34edf271cbec5c6c22cf9330e
shashishajin <t107590021@ntut.org.tw>
2020-06-21 01:35:11 +0800

    完成附錄

--- game4.10/Source/NPC.cpp
+++ game4.10/Source/NPC.cpp
@@ -5,16 +5,12 @@
 #include "gamelib.h"
 #include "gameMap.h"
 #include "NPC.h"
-
-
 namespace game_framework {
-
 	/////////////////////////////////////////////////////////////////////////////
 	// NPC: NPC base class
 	/////////////////////////////////////////////////////////////////////////////
 	NPC::NPC(gameMap* pointer,int x, int y) : currentMap(pointer), x(x), y(y) {}
 	NPC::~NPC() {}
-
 	void NPC::SetHeroState(int x1, int x2, int y1, int y2, int HP, int Gold, int AttackDamage, int Level)
 	{
 		hero["x1"] = x1;
@@ -26,35 +22,27 @@ namespace game_framework {
 		hero["AttackDamage"] = AttackDamage;
 		hero["HeroLevel"] = Level;
 	}
-
-
 	/////////////////////////////////////////////////////////////////////////////
 	// NPC_oldMan: NPC old man class
 	/////////////////////////////////////////////////////////////////////////////
 	NPC_oldMan::NPC_oldMan(gameMap* pointer, int x, int y) : NPC(pointer, x, y) {}
-
 	NPC_oldMan::~NPC_oldMan() {}
-
 	int NPC_oldMan::GetX1() 
 	{ 
 		return x; 
 	}
-
 	int NPC_oldMan::GetY1() 
 	{ 
 		return y; 
 	}
-
 	int NPC_oldMan::GetX2() 
 	{ 
 		return x + animation.Width(); 
 	}
-
 	int NPC_oldMan::GetY2() 
 	{ 
 		return y + animation.Height(); 
 	}
-
 	void NPC_oldMan::LoadBitmap()
 	{
 		animation.AddBitmap(IDB_OLD_MAN_1, RGB(255, 0, 0));
@@ -64,7 +52,6 @@ namespace game_framework {
 		InteractionBar.AddBitmap(IDB_TALK_BAR_2, RGB(255, 255, 255));
 		InteractionBar.AddBitmap(IDB_TALK_BAR_3, RGB(255, 255, 255));
 		InteractionBar.AddBitmap(IDB_TALK_BAR_4, RGB(255, 255, 255));
-
 		LevelUpBar.LoadBitmap(IDB_UI_LEVEL_UP_BAR);
 		PauseBar.LoadBitmap(IDB_UI_QUIT);
 		LevelUpInterface.LoadBitmap(IDB_UI_LEVEL_UP_INTERFACE);
